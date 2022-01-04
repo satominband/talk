be50ba62f6f7413398b6560636c7eb49c148f12f
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-01-04 11:47:36 +0900

    チャットボットAPIと感情解析APIを実装

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
-$url ='http://ap.mextractr.net/ma9/emotion_analyzer?apikey=A01B9C7259FDEFFD5FD68A3F27CD46B9B6C10A68&out=atom&text=iPhone4s%E3%81%AF%E7%94%BB%E8%';
+$text = $_GET['text'];
+$encode_text = urlencode($text);
+$url ="http://ap.mextractr.net/ma9/emotion_analyzer?apikey=A01B9C7259FDEFFD5FD68A3F27CD46B9B6C10A68&out=json&text={$encode_text}";
+
+// curlの処理を始める合図
+$curl = curl_init($url);
+
+// リクエストのオプションをセットしていく
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET'); // メソッド指定
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 証明書の検証を行わない
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // レスポンスを文字列で受け取る
+
+//API実行
+$result = curl_exec($curl);
+
+curl_close($curl);
+var_dump($result);
+$rec = json_decode($result,true);
+echo $rec;
+
+?>
